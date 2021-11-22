35567e66286d0e7aee91feeaa3e0c38c57d7dc48
nmehanma <nmehanma@gmail.com>
2021-11-22 01:23:15 -0500

    debugging query vs body

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const phoneNumberRegex = /^\d{10}$/;
 const emailRegex = /^[\w\d\S]+@[a-z\d-]+\.[a-z]{2,8}$/;
 
 //address validation
-const addressRegex = /^\d+\s[a-z]+\s[a-z]+$/;
+const addressRegex = /^\d+\s[A-Z][a-z]+\s[A-Z][a-z]+/;
 
 //city validation
 const cityRegex = /^[A-Z][a-z]+?(\s[A-Z][a-z]+|)$/;
@@ -35,9 +35,6 @@ const medicalTapeRegex = /^\d+$/;
 
 //chalk validation
 const chalkRegex = /^\d+$/;
-
-
-
 
 // function to check a string using regex
 const checkRegex = (userInput, regex) => {
@@ -59,7 +56,7 @@ const customNameValidation = value => {
 // phone number validation
 
 const customPhoneValidation = value => {
-  if (!checkRegex(value, phoneRegex)) {
+  if (!checkRegex(value, phoneNumberRegex)) {
     throw new Error("Please enter correct format: 1231231234");
   }
   return true;
@@ -95,7 +92,7 @@ const customCityValidation = value => {
 // medicalTape validation
 const customMedicalTapeValidation = value => {
   if (!checkRegex(value, medicalTapeRegex)) {
-    throw new Error("Please enter a whole number 0 or greater");
+    throw new Error("Please enter a whole number for medical Tape 0 or greater");
   }
   return true;
 };
@@ -103,11 +100,10 @@ const customMedicalTapeValidation = value => {
 // chalk valdiation
 const customChalkValidation = value => {
   if (!checkRegex(value, chalkRegex)) {
-    throw new Error("Please enter a whole number 0 or greater");
+    throw new Error("Please enter a whole number for chalk 0 or greater");
   }
   return true;
 };
-
 
 // home page root directory
 myApp.get("/", function(req, res) {
@@ -118,23 +114,24 @@ myApp.get("/", function(req, res) {
 myApp.post(
   "/",
   [
-    check("name", '').custom(customNameValidation),
-    check("address", '').custom(customAddressValidation),
-    check("city", '').custom(customCityValidation),
+    check("name", "").custom(customNameValidation),
+    check("address", "").custom(customAddressValidation),
+    check("city", "").custom(customCityValidation),
     check("province", "Province is required!").notEmpty(),
-    check("phoneNumber", '').custom(customPhoneValidation),
-    check("email", '').custom(customEmailValidation),
-    check("medicalTape", '').custom(customMedicalTapeValidation),
-    check("chalk", '').custom(customChalkValidation)
+    check("phoneNumber", "").custom(customPhoneValidation),
+    check("email", "").custom(customEmailValidation),
+    check("medicalTape", "").custom(customMedicalTapeValidation),
+    check("chalk", "").custom(customChalkValidation)
   ],
   function(req, res) {
     const errors = validationResult(req);
-    console.log(errors);
+    // console.log(errors);
 
     if (!errors.isEmpty()) {
       res.render("form", {
         errors: errors.array()
       });
+      console.log(errors)
     } else {
       let name = req.body.name;
       let address = req.body.address;
@@ -146,117 +143,130 @@ myApp.post(
       let chalk = req.body.chalk;
       let gymnasticSuit = req.body.gymnasticSuit;
       let gymnasticGrip = req.body.gymnasticGrip;
+
+   
+
+      // ***logic for form***
+
+      //cost of medicalTape
+      let medicalTapeCost = medicalTape * 3;
+
+      //cost of chalk
+      let chalkCost = chalk * 2;
+
+      console.log(gymnasticSuit)
+
+      //cost of gymnasticSuit
+      switch (gymnasticSuit) {
+        case "s":
+          gymnasticSuitCost = 2;
+          break;
+        case "m":
+          gymnasticSuitCost = 3;
+          break;
+        case "l":
+          gymnasticSuitCost = 5;
+          break;
+        default:
+          gymnasticSuitCost = 0;
+      }
+
+      //cost of gymnasticGrip
+      switch (gymnasticGrip) {
+        case "s":
+          gymnasticGripCost = 2;
+          break;
+        case "m":
+          gymnasticGripCost = 3;
+          break;
+        case "l":
+          gymnasticGripCost = 5;
+          break;
+        default:
+          gymnasticGripCost = 0;
+      }
+
+      // // array of provinces
+      // let canadianProvinces = [
+      //   "bc",
+      //   "ab",
+      //   "sk",
+      //   "mb",
+      //   "on",
+      //   "qb",
+      //   "ns",
+      //   "nb",
+      //   "pe",
+      //   "nf"
+      // ];
+
+      //determine taxRate per province selected
+
+      let taxRate = 0;
+
+      switch (province) {
+        case "bc":
+          taxRate = 0.12;
+          break;
+        case "ab":
+          taxRate = 0.05;
+          break;
+        case "sk":
+          taxRate = 0.11;
+          break;
+        case "mb":
+          taxRate = 0.12;
+          break;
+        case "on":
+          taxRate = 0.13;
+          break;
+        case "qb":
+          taxRate = 0.14975;
+          break;
+        case "ns":
+          taxRate = 0.15;
+          break;
+        case "nb":
+          taxRate = 0.15;
+          break;
+        case "pe":
+          taxRate = 0.15;
+          break;
+        case "nf":
+          taxRate = 0.15;
+      }
+
+      let subTotal =
+        medicalTapeCost + chalkCost + gymnasticSuitCost + gymnasticGripCost;
+
+      let totalTax = subTotal * taxRate;
+
+      let totalAmount = subTotal + totalTax;
+
+    
+      // if (totalAmount < 10){
+      //   return "Minimum purchase should be $10"
+      // }
+
+      let pageData = {
+        name: name,
+        address: address,
+        city: city,
+        province: province,
+        phoneNumber: phoneNumber,
+        email: email,
+        medicalTape: medicalTape,
+        chalk: chalk,
+        gymnasticSuit: gymnasticSuit,
+        gymnasticGrip: gymnasticGrip,
+        subTotal: subTotal,
+        totalTax: totalTax,
+        totalAmount: totalAmount
+      };
+      console.log(pageData);
+
+      res.render("form", pageData);
     }
-
-    // ***logic for form***
-
-    //cost of medicalTape
-    let medicalTapeCost = medicalTape * 3;
-
-    //cost of chalk
-    let chalkCost = chalk * 2;
-
-    //cost of gymnasticSuit
-    switch (gymnasticSuit) {
-      case "s":
-        gymnasticSuitCost = 2;
-        break;
-      case "m":
-        gymnasticSuitCost = 3;
-        break;
-      case "l":
-        gymnasticSuitCost = 5;
-        break;
-      default:
-        gymnasticSuitCost = 0;
-    }
-
-    //cost of gymnasticGrip
-    switch (gymnasticGrip) {
-      case "s":
-        gymnasticGripCost = 2;
-        break;
-      case "m":
-        gymnasticGripCost = 3;
-        break;
-      case "l":
-        gymnasticGripCost = 5;
-        break;
-      default:
-        gymnasticGripCost = 0;
-    }
-
-    // array of provinces
-    let canadianProvinces = [
-      "bc",
-      "ab",
-      "sk",
-      "mb",
-      "on",
-      "qb",
-      "ns",
-      "nb",
-      "pe",
-      "nf"
-    ];
-
-    //determine taxRate per province selected
-
-    let taxRate = 0;
-
-    switch (province) {
-      case "bc":
-        taxRate = 0.12;
-        break;
-      case "ab":
-        taxRate = 0.05;
-        break;
-      case "sk":
-        taxRate = 0.11;
-        break;
-      case "mb":
-        taxRate = 0.12;
-        break;
-      case "on":
-        taxRate = 0.13;
-        break;
-      case "qb":
-        taxRate = 0.14975;
-        break;
-      case "ns":
-        taxRate = 0.15;
-        break;
-      case "nb":
-        taxRate = 0.15;
-        break;
-      case "pe":
-        taxRate = 0.15;
-        break;
-      case "nf":
-        taxRate = 0.15;
-    }
-
-    // let totalTax = subTotalCost * taxRate;
-    // let totalCost = subTotalCost + totalTax;
-
-    let pageData = {
-      name: name,
-      address: address,
-      city: city,
-      province: province,
-      phoneNumber: phoneNumber,
-      email: email,
-      medicalTape: medicalTape,
-      chalk: chalk,
-      gymnasticSuit: gymnasticSuit,
-      gymnasticGrip: gymnasticGrip
-      // subTotal : subTotal,
-      // totalTax : totalTax,
-      // totalCost : totalCost
-    };
-    console.log(pageData);
-
-    res.render("form", pageData);
   }
 );
 
